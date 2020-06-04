504a52e217e077cde457936a8d33a48cde7fbf10
Eder Campos Ribeiro <edercribeiro@gmail.com>
2020-06-04 16:20:15 -0300

    Projeto criado

---
+++ src/test/java/enviando/mail/AppTest.java
@@ -0,0 +1,17 @@
+package enviando.mail;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	
+	@Test
+	public void testeEmail() {
+		
+		/*Olhe as configurações smtp do seu email*/
+		
+	}
+	
+}
